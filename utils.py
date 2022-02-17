a86399907273eacb51349b7d86e80292077b8546
aryanghbd <54407419+aryanghbd@users.noreply.github.com>
2022-02-17 16:10:49 +0000

    Pushing functional changes to revolving pi.

---
+++ utils.py
@@ -0,0 +1,74 @@
+import smbus2
+from time import sleep
+
+#ESTABLISHING CONSTANT REGISTERS#
+
+device_addr = 0x18
+
+ctrl_reg1 = 0x20 #These registers are how we initialise
+ctrl_reg4 = 0x00
+
+x_reg_high = 0x29 #These registers give the high and low nibbles of the relevant axis information
+x_reg_low = 0x28
+
+y_reg_high = 0x2B
+y_reg_low = 0x2A
+
+z_reg_high = 0x2D
+z_reg_low = 0x2C
+
+#Helpful global variables
+axis = None
+revolutions = 0
+#bus = smbus2.SMBus(1)
+#bus.write_byte_data(0x18, 0x20, 0xA7) #Run accelerometer at 100Hz
+#bus.write_byte_data(0x18, 0x23, 0x00)
+
+def reset():
+    revolutions = 0
+    axis = None:
+
+def initialize():
+    bus = smbus2.SMBus(1)
+    bus.write_byte_data(device_addr, ctrl_reg1, 0xA7) #A7 allows 100Hz standard operation enabling all 3 axes
+    bus.write_byte_data(device_addr, ctrl_reg4, 0x00) #Standard operation
+
+    print("Starting Up toilet.io...Please Wait")
+    sleep(2)
+    return bus
+
+def normalize(bus, device, register):
+    lsb = bus.read_byte_data(device, register)
+    msb = bus.read_byte_data(device, register + 1)
+    res = (msb << 8 | lsb)
+    if res > 32768:
+        res = res - 65536
+    return (res/16380) * 9.8
+
+def get_X(bus):
+    return normalize(bus, device_addr, x_reg_low)
+
+def get_Y(bus):
+    return normalize(bus, device_addr, y_reg_low)
+
+def get_Z(bus):
+    return normalize(bus, device_addr, z_reg_low)
+
+def main():
+
+    bus = initialize()
+    while True:
+        X = get_X(bus)
+        Y = get_Y(bus)
+        Z = get_Z(bus)
+        if Z < -7.5 and axis == None:
+            axis = Z
+        if Z > 8 and axis != None:
+            revolutions = revolutions + 1
+            print("Rolled! Number of revolutions: " + str(revolutions))
+            print("You have now used: " + str(revolutions * 2) + " sheets of toilet paper.")
+            axis = None
+        sleep(0.01)
+
+if __name__ == "__main__":
+    main()
