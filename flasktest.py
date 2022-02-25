7c8cacd0bfb3b2a7227862ea904ddaeba2756d6f
aryanghbd <aryantech10@yahoo.com>
2022-02-25 17:24:19 +0000

    Testing server

--- flasktest.py
+++ flasktest.py
@@ -17,7 +17,7 @@ def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
-
+    #Test
     # Start our TwiML response
     resp = MessagingResponse()
 
