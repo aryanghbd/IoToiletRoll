869ed30f96a74fd7b20e39d0f9fb5f1c35f3bc1d
aryanghbd <aryantech10@yahoo.com>
2022-02-25 17:21:35 +0000

    Testing server

--- flasktest.py
+++ flasktest.py
@@ -1,17 +1,17 @@
+import flask
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 
-
 app = Flask(__name__)
-account_sid = 'AC552ea4e452978a40ad8d0061fc83e077'
-auth_token = '244e9e2bc5d559fbc125ef58a2edc70a'
-client = Client(account_sid, auth_token)
 
 
-@app.route("/")
-
-
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    flask.render_template("serverresponse.html")
+    while True:
+        print("what's up")
+    return 0
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
@@ -24,12 +24,10 @@ def incoming_sms():
     # Determine the right reply for this message
     if body == 'YES':
         resp.message("Meme generated")
-        utils.generate_meme(utils.current_msg, utils.current_sheets)
     elif body == 'NO':
         resp.message(":D")
 
     return str(resp)
-
 
 if __name__ == "__main__":
     app.run(debug=False)
--- utils.py
+++ utils.py
@@ -261,6 +261,24 @@ mqtt_client.subscribe("IC.embedded/Useless_System/Household")
 mqtt_client.on_message = on_message
 mqtt_client.loop_start()
 
+@app.route("/sms", methods=['GET', 'POST'])
+def incoming_sms():
+    """Send a dynamic reply to an incoming text message"""
+    # Get the message the user sent our Twilio number
+    body = request.values.get('Body', None)
+
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Determine the right reply for this message
+    if body == 'YES':
+        resp.message("Meme generated")
+    elif body == 'NO':
+        resp.message(":D")
+
+    return str(resp)
+
+@app.route("/")
 def main():
     if not check_for_household():
         print("Waiting for household to be input")
