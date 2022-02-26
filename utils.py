bf4cccdc687b5083e7b9ffbb5497600a075c1fec
aryanghbd <aryantech10@yahoo.com>
2022-02-26 22:07:12 +0000

    Testing server

--- utils.py
+++ utils.py
@@ -245,7 +245,7 @@ def synch_wait(time):
 def on_message(client, userdata, message):
     global current_msg, household, start_flag
     current_msg = str(message.payload.decode("utf-8"))
-    if len(current_msg.split()) == 1 and not check_for_household():
+    if len(current_msg.split()) == 1 and check_for_household():
         start_flag = check_user(current_msg)
     if message.topic == "IC.embedded/Useless_System/Household" and not os.path.isfile('household.json'):
         household = (json.loads(current_msg))
